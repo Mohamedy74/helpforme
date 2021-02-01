ce365ccb5ca328aff1cd6954a017bd137d931666
Mohamedy74 <mohamedymohamed60@gmail.com>
2021-02-02 00:51:44 +0200

    edit call us

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,6 +18,46 @@
 
 
     $(document).ready(function () {
+        /* Tabs */
+        $('.tabs a').on('click', function () {
+            var link = $(this).attr('data-link');
+            var pane = $('[data-pane="' + link + '"]');
+            $('.tabs__link.active').removeClass('active');
+            $(this).addClass('active');
+            $('.tabs__pane.active').removeClass('active');
+            pane.addClass('active');
+        });
+
+        /*File Up*/
+        $.fileup({
+            url: '/file/upload',
+            inputID: 'upload-2',
+            dropzoneID: 'upload-2-dropzone',
+            queueID: 'upload-2-queue',
+            lang: 'ru',
+            onSelect: function (file) {
+                $('#multiple button').show();
+            },
+            onRemove: function (file, total) {
+                if (file === '*' || total === 1) {
+                    $('#multiple button').hide();
+                }
+            },
+            onSuccess: function (response, file_number, file) {
+                Snarl.addNotification({
+                    title: 'Upload success',
+                    text: file.name,
+                    icon: '<i class="fa fa-check"></i>'
+                });
+            },
+            onError: function (event, file, file_number) {
+                Snarl.addNotification({
+                    title: 'Upload error',
+                    text: file.name,
+                    icon: '<i class="fa fa-times"></i>'
+                });
+            }
+        });
 
         /*START AOS JS*/
         AOS.init({
